c0a7e4a670923bc1f55ea7b8b5523146fe22b391
Andrew Ahn <aeahn2@illinois.edu>
2019-12-02 23:31:35 -0600

    Testing Studio

--- app/src/main/java/com/example/finalproject/NewGameActivity.java
+++ app/src/main/java/com/example/finalproject/NewGameActivity.java
@@ -2,7 +2,9 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -14,25 +16,35 @@ import android.widget.RadioGroup;
  */
 
 public final class NewGameActivity extends AppCompatActivity {
+    private static final String TAG = "NewGameActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "NewGameActivity");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game);
 
 //        RadioGroup gameMode = findViewById(R.id.gameMode);
+
+
 //        RadioButton beginner = findViewById(R.id.beginner);
 //
 //        RadioButton advanced = findViewById(R.id.advanced);
 //
-//        Button startGame = findViewById(R.id.startGame);
+        Button startGame = findViewById(R.id.startGame);
 //
-//        startGame.setOnClickListener(unused -> createGameClicked());
+        startGame.setOnClickListener(createGameClicked());
 
 
 
     }
 
-//    private void createGameClicked() {
-//        g
-//    }
+    private void createGameClicked() {
+        RadioGroup gameMode = findViewById(R.id.gameMode);
+        Intent intent = new Intent(this, GameActivity.class);
+        if (gameMode.getCheckedRadioButtonId() == R.id.beginner) {
+            Log.i(TAG, "beginner button pressed");
+        } else if (gameMode.getCheckedRadioButtonId() == R.id.advanced) {
+            Log.i(TAG, "advanced button clicked");
+        }
+    }
 }
