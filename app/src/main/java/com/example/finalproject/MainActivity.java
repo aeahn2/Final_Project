02258c3cd34a2ffc6105ad05473139911c0d7949
Andrew Ahn <aeahn2@illinois.edu>
2019-12-02 21:13:24 -0600

    commit message

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     //test
+    //test 2
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
