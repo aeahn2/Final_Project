41d701cd210d6f33826614000517182f1a0b28ac
sugaronsnow <anniekli01@gmail.com>
2019-12-05 19:57:24 -0600

    Still to do: -change how date is displayed? -change how each activity affects progress bars? -USE WEB API/LIBRARY!!!! -CREATE ACTIVITY LOG

--- app/src/main/java/com/example/finalproject/GameActivity.java
+++ app/src/main/java/com/example/finalproject/GameActivity.java
@@ -65,11 +65,6 @@ public final class GameActivity extends AppCompatActivity {
 
 
 //    Work on MP (Mental Health -6, Grades +8, Social Life -10, CS Ability +6)
-//    Do PrairieLearn (Mental Health 0, Grades +2, Social Life 0, CS Ability +2)
-//    Spend time with friends (Mental Health +4, Grades -2, Social Life +3, CS Ability -2)
-//    Go to party (Mental Health +6, Grades -6, Social Life +6, CS Ability -6)
-
-
     public void workMPMethod() {
         ProgressBar gradesBar = findViewById(R.id.gradesBar);
         ProgressBar abilityBar = findViewById(R.id.abilityBar);
@@ -87,6 +82,8 @@ public final class GameActivity extends AppCompatActivity {
 
     }
 
+
+    //    Spend time with friends (Mental Health +4, Grades -2, Social Life +3, CS Ability -2)
     public void friendsMethod() {
         ProgressBar gradesBar = findViewById(R.id.gradesBar);
         ProgressBar abilityBar = findViewById(R.id.abilityBar);
@@ -94,15 +91,16 @@ public final class GameActivity extends AppCompatActivity {
         ProgressBar socialBar = findViewById(R.id.socialBar);
         TextView currentDate = findViewById(R.id.date);
 
-        gradesBar.incrementProgressBy(8);
-        abilityBar.incrementProgressBy(6);
-        mentalBar.incrementProgressBy(-6);
-        socialBar.incrementProgressBy(-10);
+        gradesBar.incrementProgressBy(0);
+        abilityBar.incrementProgressBy(-2);
+        mentalBar.incrementProgressBy(+4);
+        socialBar.incrementProgressBy(+3);
 
         date++;
         currentDate.setText(Integer.toString(date));
     }
 
+    //    Do PrairieLearn (Mental Health 0, Grades +2, Social Life 0, CS Ability +2)
     public void prairieLearnMethod() {
         ProgressBar gradesBar = findViewById(R.id.gradesBar);
         ProgressBar abilityBar = findViewById(R.id.abilityBar);
@@ -110,16 +108,17 @@ public final class GameActivity extends AppCompatActivity {
         ProgressBar socialBar = findViewById(R.id.socialBar);
         TextView currentDate = findViewById(R.id.date);
 
-        gradesBar.incrementProgressBy(8);
-        abilityBar.incrementProgressBy(6);
-        mentalBar.incrementProgressBy(-6);
-        socialBar.incrementProgressBy(-10);
+        gradesBar.incrementProgressBy(2);
+        abilityBar.incrementProgressBy(2);
+        mentalBar.incrementProgressBy(0);
+        socialBar.incrementProgressBy(0);
 
         date++;
         currentDate.setText(Integer.toString(date));
 
     }
 
+    //    Go to party (Mental Health +6, Grades -6, Social Life +6, CS Ability -6)
     public void partyMethod() {
         ProgressBar gradesBar = findViewById(R.id.gradesBar);
         ProgressBar abilityBar = findViewById(R.id.abilityBar);
@@ -127,10 +126,10 @@ public final class GameActivity extends AppCompatActivity {
         ProgressBar socialBar = findViewById(R.id.socialBar);
         TextView currentDate = findViewById(R.id.date);
 
-        gradesBar.incrementProgressBy(8);
-        abilityBar.incrementProgressBy(6);
-        mentalBar.incrementProgressBy(-6);
-        socialBar.incrementProgressBy(-10);
+        gradesBar.incrementProgressBy(-6);
+        abilityBar.incrementProgressBy(-6);
+        mentalBar.incrementProgressBy(6);
+        socialBar.incrementProgressBy(-6);
 
         date++;
         currentDate.setText(Integer.toString(date));
